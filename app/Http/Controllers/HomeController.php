48c948a79603944015bfa85f0108b2853f9ef425
Danilo De La Cruz <daklo.info@gmail.com>
2020-02-15 21:28:03 -0400

    Home controller is modified so it can handle the session chooser's view request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
@@ -25,4 +27,32 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+	public function chooseCurrentSession(Request $request) 
+	{
+
+		$user          = Auth::user();
+		$sessionId     = Session::getId(); //get the new session id after user signed in
+		$lastSessionId = $user->last_session_id;
+
+		if ( ! empty( $lastSessionId ) ) {
+
+			$lastSession                = Session::getHandler()->read( $lastSessionId );
+			$continueWithCurrentSession = ( $request->continue == 'yes' ) ? true : false;
+
+			if ( $continueWithCurrentSession && $lastSession ) {
+				//if there was an active last session, then it's closed
+				Session::getHandler()->destroy( $lastSessionId );
+				Session::forget( 'multiple_session_flag' );
+
+			} else {
+				return view( 'auth.sessionChooser' );
+			}
+		}
+
+		$user->last_session_id = $sessionId;
+		$user->save();
+
+		return redirect( 'login' );
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/login/continue', 'HomeController@chooseCurrentSession')->name('login.continue');
